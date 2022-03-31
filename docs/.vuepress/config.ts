d333eb61a4713a7af4868b60841430c4c92de35d
xiyan <liuzhi0621@gmail.com>
2022-03-31 23:53:46 +0800

    feat: 项目初始化

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,32 @@
+import { defineHopeConfig } from "vuepress-theme-hope";
+import themeConfig from "./themeConfig";
+
+export default defineHopeConfig({
+  base: "/",
+
+  dest: "./dist",
+
+  host: "localhost",
+
+  port: 30000,
+
+  head: [
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
+      },
+    ],
+  ],
+
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "主题演示",
+      description: "vuepress-theme-hope 的演示",
+    },
+  },
+
+  themeConfig,
+});
